25e1537f37f14009be1a85b0faf6f879cde23a26
truelGit <ivichev@gmail.com>
2025-02-19 15:38:44 +0100

    Add project files.

---
+++ GrpcClient/Program.cs
@@ -0,0 +1,22 @@
+﻿using Grpc.Net.Client;
+using GrpcServer;
+
+namespace GrpcClient
+{
+	internal class Program
+	{
+		static async Task Main(string[] args)
+		{
+			var input = new HelloRequest { Name = "Alex" };
+
+			var channel = GrpcChannel.ForAddress("https://localhost:7071");
+			var client = new Greeter.GreeterClient(channel);
+
+			var reply = await client.SayHelloAsync(input);
+
+			Console.WriteLine(reply.Message);
+
+			Console.ReadLine();
+		}
+	}
+}
